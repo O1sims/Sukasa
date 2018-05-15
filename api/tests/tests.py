098eca01758e1f0e665263facce1aee206ceb95b
Owen Sims <owen.sims@cyberlytic.com>
2018-05-15 23:30:22 +0100

    Add basic data to test analytics

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,6 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import pickle
+import unittest
 
-# Create your tests here.
+from analytics.utilities.CollectPropertyData import get_final_page_number, get_all_main_images
+
+
+class TestingPropertyCollection(unittest.TestCase):
+
+    def test_final_page_number(self):
+        with open('/home/owen/Code/Kasa/analytics/data/test_page.pkl') as f:
+            first_page = pickle.load(f)
+        page_number = get_final_page_number(
+                first_page_soup=first_page)
+        self.assertEqual(page_number, 54)
+
+    def test_get_all_main_images(self):
+        with open('/home/owen/Code/Kasa/analytics/data/test_page.pkl') as f:
+            page_soup = pickle.load(f)
+        main_images = get_all_main_images(page_soup=page_soup)
+        self.assertEqual(len(main_images), 10)
+        self.assertTrue(isinstance(main_images, list))
+        self.assertTrue(isinstance(main_images[0], str))
+
+if __name__ == '__main__':
+    unittest.main()
