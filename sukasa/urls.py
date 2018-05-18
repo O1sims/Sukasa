3e5af5d15da5e14503922e3a8718ddbdbbffcad6
Owen Sims <owen.sims@cyberlytic.com>
2018-05-18 16:09:54 +0100

    Add the Swagger URL

--- sukasa/urls.py
+++ sukasa/urls.py
@@ -19,9 +19,13 @@ from api.views.index import IndexView
 from api.views.collect_property_data import CollectPropertyDataView
 
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework_swagger.views import get_swagger_view
 
 
 urlpatterns = format_suffix_patterns([
+    url(r'^swagger/$',
+        get_swagger_view(
+            title='Sukasa API')),
 
     url(r'^application/collect_property_data/$',
         CollectPropertyDataView.as_view()),
