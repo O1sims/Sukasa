86d8a210a3f4fae7fb0989beca40387a6861b0a7
Owen Sims <owen.sims@cyberlytic.com>
2018-05-18 15:55:52 +0100

    Have choices for sorting through the API

--- api/models/collect_property_data.py
+++ api/models/collect_property_data.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 
 
+SORTING_CHOICES = (
+    "mostPopular",
+    "recentlyAdded",
+    "recentlyUpdated",
+    "priceLowHigh",
+    "priceHighLow"
+)
+
+
 class CollectPropertyDataModel(serializers.Serializer):
     area = serializers.CharField()
     propertyType = serializers.CharField()
-    sortBy = serializers.CharField()
+    sortBy = serializers.ChoiceField(choices=SORTING_CHOICES)
