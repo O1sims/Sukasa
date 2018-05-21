5e3bbc1efa17e7356ab467234a895c672f437f3f
Owen Sims <owen.sims@cyberlytic.com>
2018-05-21 22:39:00 +0100

    Elaborate on property model

--- api/models/property_data.py
+++ api/models/property_data.py
@@ -21,15 +21,62 @@ class GeneratePropertyDataModel(serializers.Serializer):
     sortBy = serializers.ChoiceField(choices=SORTING_CHOICES)
 
 
+class PropertyLocationModel(serializers.Serializer):
+    lat = serializers.FloatField(required=False)
+    lon = serializers.FloatField(required=False)
+
+
+class BandScoreModel(serializers.Serializer):
+    band = serializers.CharField(required=False)
+    score = serializers.IntegerField(required=False)
+
+
+class EPCChartModel(serializers.Serializer):
+    actual = BandScoreModel(required=False)
+    potential = BandScoreModel(required=False)
+
+
+class PropertyAmenitiesModel(serializers.Serializer):
+    garage = serializers.BooleanField(required=False)
+    parking = serializers.BooleanField(required=False)
+    garden = serializers.BooleanField(required=False)
+    driveway = serializers.BooleanField(required=False)
+    bayWindow = serializers.BooleanField(required=False)
+
+
 class PropertyDetailModel(serializers.Serializer):
+    style = serializers.CharField(required=False)
+    status = serializers.CharField(required=False)
     bedrooms = serializers.IntegerField(required=False)
     bathrooms = serializers.IntegerField(required=False)
-    garage = serializers.BooleanField(required=False)
+    location = PropertyLocationModel(required=False)
+    amenities = PropertyAmenitiesModel(required=False)
+    epcChart = EPCChartModel(required=False)
+
+
+class EstateAgentModel(serializers.Serializer):
+    branch = serializers.CharField(required=False)
+    name = serializers.CharField(required=False)
+
+
+class PriceInformationModel(serializers.Serializer):
+    maxPrice = serializers.IntegerField(required=False)
+    minPrice = serializers.IntegerField(required=False)
+    currency = serializers.CharField(required=False)
+    price = serializers.IntegerField(required=False)
+    offer = serializers.CharField(required=False)
 
 
 class GetPropertyDataModel(serializers.Serializer):
     _id = serializers.CharField(required=False)
+    timestamp = serializers.DateTimeField(required=False)
+    property_id = serializers.CharField(required=False)
+    brief = serializers.CharField(required=False)
     address = serializers.CharField(required=False)
+    town = serializers.CharField(required=False)
     area = serializers.CharField(required=False)
+    image = serializers.CharField(required=False)
     postcode = serializers.CharField(required=False)
+    hyperlink = serializers.CharField(required=False)
     details = PropertyDetailModel(required=False)
+    estateAgent = EstateAgentModel(required=False)
