0b04ce5b322458feb49a2de73d51b9c156782b7b
Owen Sims <owen.sims@cyberlytic.com>
2018-06-03 22:16:54 +0100

    Add postcode-location dictionary

---
+++ analytics/utilities/LocationalDetails.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  8 11:06:05 2018
+
+@author: owen
+"""
+
+
+postcode_areas = {
+    'BT1': 'Central Belfast',
+    'BT2': 'Central Belfast',
+    'BT3': 'Central Belfast',
+    'BT4': 'East Belfast',
+    'BT5': 'East Belfast',
+    'BT6': 'East Belfast',
+    'BT7': 'Central Belfast',
+    'BT8': 'South Belfast',
+    'BT9': 'South Belfast',
+    'BT10': 'South Belfast',
+    'BT11': 'West Belfast',
+    'BT12': 'West Belfast',
+    'BT13': 'North Belfast',
+    'BT14': 'North Belfast',
+    'BT15': 'Central Belfast',
+    'BT16': 'East Belfast',
+    'BT17': 'South Belfast',
+    'BT18': 'Holywood',
+    'BT19': 'Bangor',
+    'BT20': 'Bangor',
+    'BT21': 'Donaghadee'
+}
