46198e8d2001bdf30e40e440e9555657da9945c2
Owen Sims <owen.sims@cyberlytic.com>
2018-06-16 15:56:45 +0100

    Add some environmental variable indicating the API version

--- gui/app/search/search.service.ts
+++ gui/app/search/search.service.ts
@@ -1,4 +1,5 @@
 import { Http, Response, Request, Headers, RequestOptions, RequestMethod, URLSearchParams } from "@angular/http";
+import { environment } from '../environment/environment';
 import { Injectable } from '@angular/core';
 
 import 'rxjs/add/operator/map';
@@ -6,8 +7,10 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchService {
+  apiVerion:string = environment.API_VERSION;
 
-  constructor(private http: Http) {
+  constructor(
+    private http: Http) {
 	};
 
   searchProperties(propertyType, searchQuery) {
@@ -15,7 +18,7 @@ export class SearchService {
     params.set('q', searchQuery);
     var requestoptions = new RequestOptions({
 			method: RequestMethod.Get,
-			url: '/api/v1/properties/' + propertyType + '/',
+			url: '/api/v' + this.apiVerion + '/properties/' + propertyType + '/',
       params: params
 		});
 		return this.http.request(
