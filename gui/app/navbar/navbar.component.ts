1f7ffa58c9aa3f30235dcbfebee76d921c9a639a
Owen Sims <owen.sims@cyberlytic.com>
2018-07-10 14:43:21 +0100

    Update navbar

--- gui/app/navbar/navbar.component.ts
+++ gui/app/navbar/navbar.component.ts
@@ -15,14 +15,10 @@ export class NavBarComponent implements OnInit {
 
   getPathname() {
     this.pathName = window.location.pathname;
-    if (this.pathName == '/' || this.pathName == '/risk-graph') {
-      this.selectedTab = 'riskNetwork';
-    } else if (this.pathName == '/cvi' || this.pathName == '/cvi/') {
-      this.selectedTab = 'cvi';
-    } else if (this.pathName == '/risk-appetite' || this.pathName == '/risk-appetite/') {
-      this.selectedTab = 'riskAppetite';
-    } else if (this.pathName == '/actions' || this.pathName == '/actions/') {
-      this.selectedTab = 'actions';
+    if (this.pathName == '/' || this.pathName == '/sale') {
+      this.selectedTab = 'sale';
+    } else if (this.pathName == '/rent' || this.pathName == '/rent/') {
+      this.selectedTab = 'rent';
     };
   };
 
