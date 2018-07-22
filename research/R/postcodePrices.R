b58d11f926d8beca41403c789fa36133b594d0aa
Owen Sims <owen.sims@cyberlytic.com>
2018-07-22 12:05:55 +0100

    Clean up postcode price analysis

--- research/R/postcodePrices.R
+++ research/R/postcodePrices.R
@@ -1,26 +1,76 @@
 library(ggplot2)
 library(ggthemes)
+library(jsonlite)
 library(magrittr)
 
 
-postcodePrices <- function() {
+postcodePrices <- function(consolidateStyles = TRUE, removeAnomalies = FALSE) {
   propertyData <- getwd() %>%
     paste0("/research/data/propertyData.json") %>%
-    jsonlite::fromJSON() %>%
-    subset(!(priceInfo$price %in% c(NA, "POA")))
+    jsonlite::fromJSON()
   
-  postcodeTable <- propertyData$postcode %>%
-    table() %>%
-    as.data.frame(
-      stringsAsFactors = FALSE)
+  propertyData %<>% subset(!(priceInfo$price %in% c(NA, "POA"))) %>%
+    subset(!(postcode %>% is.na())) %>%
+    subset(!(postcode %in% (
+      propertyData$postcode %>%
+        table() %>%
+        as.data.frame() %>% 
+        subset(Freq <= 10) %$% .))) %>%
+    subset(!(details$style %>% is.na())) %>%
+    subset(!(details$style %in% (
+      propertyData$details$style %>%
+        table() %>%
+        as.data.frame() %>% 
+        subset(Freq <= 10) %$% .)))
   
-  rarePostcodes <- postcodeTable %>% 
-    subset(Freq <= 10) %$% .
+  if (consolidateStyles) {
+    hashKeys <- c("Apartment", "Flat", "Bungalow", "Detached", 
+                  "Semi-detached", "Terrace", "terrace", "Townhouse")
+    hashValues <- c("Apartment", "Apartment", "Bungalow", "Detached house", 
+                    "Semi-detached house", "Terrace house", "Terrace house", "Townhouse")
+    
+    consolidatedStyles <- hashmap::hashmap(
+      keys = hashKeys,
+      values = hashValues)
+    
+    for (key in hashKeys) {
+      getValues <- grepl(
+        pattern = key,
+        x = propertyData$details$style, 
+        ignore.case = FALSE)
+      propertyData$details$style[getValues] <- consolidatedStyles[[key]]
+    }
+  }
   
-  propertyData %<>% subset(!(postcode %in% rarePostcodes))
+  if (removeAnomalies) {
+    # For each postcode and each property style we remove anomalous data points
+    # Let's define an anomalous data point as some point that is a standard deviation
+    # away from the mean.
+    adjustedPropertyList <- list()
+    for (postCode in propertyData$postcode %>% unique()) {
+      subsetPropertyData <- propertyData %>% 
+        subset(postcode == postCode)
+      for (style in subsetPropertyData$details$style %>% unique()) {
+        subsetStyle <- subsetPropertyData %>%
+          subset(details$style == style)
+        meanStylePrice <- subsetStyle$priceInfo$price %>%
+          as.integer() %>%
+          mean()
+        stdDeviationPrice <- subsetStyle$priceInfo$price %>%
+          as.integer() %>%
+          sd()
+        normalPropertydata <- subsetStyle %>%
+          subset(
+            priceInfo$price %>% as.integer() <= meanStylePrice + stdDeviationPrice)
+        adjustedPropertyList %<>% 
+          rbind(normalPropertydata %>% as.list())
+      }
+    }
+    adjustedPropertyData <- adjustedPropertyList %>% as.data.frame(stringsAsFactors = FALSE)
+  }
   
-  ggplot(
-    data = propertyData,
+  postCodePlot <- ggplot(
+    data = adjustedPropertyList,
     aes(x = postcode,
         y = priceInfo$price %>% as.integer(),
         colour = details$style)) +
@@ -34,7 +84,18 @@ postcodePrices <- function() {
     theme(
       axis.text.x = element_text(
         angle = 45, 
-        hjust = 1))
+        hjust = 1)) + 
+    scale_colour_discrete(
+      name = "Property style")
   
+  getwd() %>%
+    paste0("/research/images/postcode-prices",
+           ifelse(
+             test = consolidateStyles, 
+             yes = "-consolidated", 
+             no = ""), 
+           ".png") %>%
+    ggsave()
   
+  return(postCodePlot)
 }
