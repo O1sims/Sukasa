6ebfaa5d2507e9081840286c1f46346420a25437
Owen Sims <owen.sims@cyberlytic.com>
2018-08-26 22:21:53 +0100

    Add switch to push default property data to DB

--- src/backend/sukasa/settings.py
+++ src/backend/sukasa/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+from utils.data_loader import save_default_property_data
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,5 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/app/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'gui/app'),
+    '../../backend/gui/app',
 ]
+
+# Load default NI property data
+if os.getenv('DEVELOPMENT', False):
+    save_default_property_data()
