67d9f33503e463d173765814ac6b8e0cdc0855f6
Owen Sims <owen.sims@cyberlytic.com>
2018-09-10 21:18:25 +0100

    Load data logic

--- src/backend/utils/data_loader.py
+++ src/backend/utils/data_loader.py
@@ -4,10 +4,17 @@ from utils.data.ni_property_data import DEFAULT_NI_PROPERTY_DATA
 
 
 def save_default_property_data():
+    if ElasticService().index_check(
+            index=config.ELASTICSEARCH_QUERY_INFO['propertyIndex']):
+        ElasticService().drop_database(
+            index=config.ELASTICSEARCH_QUERY_INFO['propertyIndex'])
+    else:
+        ElasticService().create_index(
+            index=config.ELASTICSEARCH_QUERY_INFO['propertyIndex'])
     ElasticService().save_to_database(
         index=config.ELASTICSEARCH_QUERY_INFO['propertyIndex'],
         doc_type=config.ELASTICSEARCH_QUERY_INFO['propertyDocType'],
-        data=DEFAULT_NI_PROPERTY_DATA)
-    print 'Added {} NI properties to the {} index!'.format(
-        len(DEFAULT_NI_PROPERTY_DATA),
-        config.ELASTICSEARCH_QUERY_INFO['propertyIndex'])
+        data=DEFAULT_NI_PROPERTY_DATA[0])
+    return (len(DEFAULT_NI_PROPERTY_DATA),
+            config.ELASTICSEARCH_QUERY_INFO['propertyIndex'],)
+
