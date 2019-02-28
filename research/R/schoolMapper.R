2a611888a6dfeb90e8ed9a5a9d880ca443613c7e
O1sims <sims.owen@gmail.com>
2019-02-28 21:27:48 +0000

    Google maps integration

--- research/R/schoolMapper.R
+++ research/R/schoolMapper.R
@@ -1,8 +1,51 @@
+library(ggmap)
 library(ggplot2)
 library(magrittr)
+library(maptools)
 
 
-allSchoolData <- "~/Code/Sukasa/research/data/school/AllNISchoolData.json" %>%
+generate_shapefile_map <- function() {
+  spdf <- getwd() %>% 
+    paste0("/research/data/map/northern-ireland/parliamentaries/NI-parliamentary-boundaries.shp") %>%
+    maptools::readShapePoly()
+  
+  spdf@data$id <- rownames(spdf@data)
+  spdf.points <- ggplot2::fortify(spdf, region = "id")
+  heatmap.data <- dplyr::inner_join(
+    spdf.points, 
+    spdf@data, 
+    by = "id")
+  
+  heatmap.data$COUNTYNAME %<>% 
+    as.vector()
+  
+  shapefileMap <- ggplot(
+    data = heatmap.data) + 
+    geom_polygon(
+      colour = "white",
+      size = 0.5, 
+      aes(
+        x = long, 
+        y = lat, 
+        group = group))
+  
+  return(shapefileMap)
+}
+
+
+generate_google_map <- function(longitude = -6, latitude = 54.5) {
+  googleMap <- c(
+    lon = longitude, 
+    lat = latitude) %>% 
+    ggmap::get_map() %>%
+    ggmap::ggmap()
+  
+  return(googleMap)
+}
+
+
+allSchoolData <- getwd() %>%
+  paste0("/research/data/school/allNISchoolData.json") %>%
   jsonlite::read_json()
 
 
@@ -20,46 +63,7 @@ for (school in allSchoolData) {
   }
 }
 
-data.frame(
-  schoolName = schoolName,
-  schoolType = schoolType,
-  latitude = latitude,
-  longitude = longitude,
-  stringsAsFactors = FALSE) %>%
-  ggplot() +
-  geom_point(
-    aes(
-      y = latitude, 
-      x = longitude, 
-      colour = schoolType)) + 
-  theme_minimal()
-
-
-
-
-
-
-spdf <- "/home/owen/Code/Sukasa/research/data/map/northern-ireland/parliamentaries/NI-parliamentary-boundaries.shp" %>%
-  maptools::readShapePoly()
-
-spdf@data$id <- rownames(spdf@data)
-spdf.points <- ggplot2::fortify(spdf, region = "id")
-heatmap.data <- dplyr::inner_join(
-  spdf.points, 
-  spdf@data, 
-  by = "id")
-
-heatmap.data$COUNTYNAME %<>% as.vector()
-
-ggplot(
-  data = heatmap.data) + 
-  geom_polygon(
-    colour = "white",
-    size = 0.5, 
-    aes(
-      x = long, 
-      y = lat, 
-      group = group)) +
+generate_shapefile_map() +
   geom_point(
     data = data.frame(
       schoolName = schoolName,
@@ -72,7 +76,11 @@ ggplot(
       x = longitude, 
       colour = schoolType, 
       alpha=0.5)) + 
-  ylab("") + xlab("") +
+  ylab("") + 
+  xlab("") +
+  coord_cartesian(
+    xlim = c(-6.5, -5.5),
+    ylim = c(54.45, 54.75)) +
   theme_minimal() + 
   theme(
     panel.border = element_blank(), 
@@ -80,5 +88,5 @@ ggplot(
     panel.grid.minor = element_blank(),
     axis.text = element_blank(),
     axis.ticks = element_blank(),
-    panel.background = element_blank())
-
+    panel.background = element_blank(),
+    legend.position = "bottom")
