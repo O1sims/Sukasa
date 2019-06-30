0c7c761f745f2e243f0cd19930d79bfc873cf0be
O1sims <sims.owen@gmail.com>
2019-06-30 19:28:31 +0100

    Add master data to the data loader

--- src/backend/utils/data_loader.py
+++ src/backend/utils/data_loader.py
@@ -5,13 +5,20 @@ from api.services.MongoService import MongoService
 
 
 def insert_default_property_data():
-    belfast_property_data = open(BASE_PATH + 'utils/data/belfastPropertyData.json')
+    belfast_property_data = open('{}utils/data/belfastPropertyData.json'.format(BASE_PATH))
+    belfast_master_data = open('{}utils/data/belfastMasterData.json'.format(BASE_PATH))
     belfast_properties = json.load(belfast_property_data)
+    master_data = json.load(belfast_property_data)
     if MongoService().check_collection(
             collection_name=MONGO_DB_INFO['propertyCollection']):
         MongoService().drop_database(
             collection_name=MONGO_DB_INFO['propertyCollection'])
+        MongoService().drop_database(
+            collection_name=MONGO_DB_INFO['masterCollection'])
     MongoService().insert_to_collection(
         collection_name=MONGO_DB_INFO['propertyCollection'],
         data=belfast_properties)
+    MongoService().insert_to_collection(
+        collection_name=MONGO_DB_INFO['masterCollection'],
+        data=belfast_master_data)
     return belfast_properties
