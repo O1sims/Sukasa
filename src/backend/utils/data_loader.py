4d88c6eca1b2e4a01140b8d671040e5c1b7f004d
O1sims <sims.owen@gmail.com>
2019-07-01 19:03:45 +0100

    Add Master data to Mongo

--- src/backend/utils/data_loader.py
+++ src/backend/utils/data_loader.py
@@ -5,14 +5,16 @@ from api.services.MongoService import MongoService
 
 
 def insert_default_property_data():
-    belfast_property_data = open('{}utils/data/belfastPropertyData.json'.format(BASE_PATH))
-    belfast_master_data = open('{}utils/data/belfastMasterData.json'.format(BASE_PATH))
+    belfast_property_data = open(BASE_PATH + 'utils/data/belfastPropertyData.json')
+    belfast_master_data = open(BASE_PATH + 'utils/data/belfastMasterData.json')
     belfast_properties = json.load(belfast_property_data)
-    master_data = json.load(belfast_property_data)
+    master_data = json.load(belfast_master_data)
     if MongoService().check_collection(
             collection_name=MONGO_DB_INFO['propertyCollection']):
         MongoService().drop_database(
             collection_name=MONGO_DB_INFO['propertyCollection'])
+    if MongoService().check_collection(
+            collection_name=MONGO_DB_INFO['masterCollection']):
         MongoService().drop_database(
             collection_name=MONGO_DB_INFO['masterCollection'])
     MongoService().insert_to_collection(
@@ -20,5 +22,5 @@ def insert_default_property_data():
         data=belfast_properties)
     MongoService().insert_to_collection(
         collection_name=MONGO_DB_INFO['masterCollection'],
-        data=belfast_master_data)
+        data=master_data)
     return belfast_properties
