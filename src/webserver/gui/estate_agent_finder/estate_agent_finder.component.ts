640bc5de21d89a72f30a5cc7b574f7f4d1c07074
O1sims <sims.owen@gmail.com>
2019-07-03 23:00:12 +0100

    Add boilerplate TS code for estate agent finder page

---
+++ src/webserver/gui/estate_agent_finder/estate_agent_finder.component.ts
@@ -0,0 +1,107 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EstateAgentFinderService } from './estate_agent_finder.service';
+import { SharedService } from '../shared/shared.service';
+
+
+@Component({
+  templateUrl: './estate_agent_finder/estate_agent_finder.component.html',
+  providers: [EstateAgentFinderService, SharedService]
+})
+
+export class EstateAgentFinderComponent implements OnInit {
+  
+  towns:string[];
+  heatingChoices:string[];
+  aggregateStyleChoices:string[];
+
+  town:string;
+  heating:string;
+  aggregateStyle:string;
+  postcode:string;
+  bedrooms:number;
+  address:string;
+
+  garden:boolean = false;
+  driveway:boolean = false;
+  garage:boolean = false;
+  bayWindow:boolean = false;
+  parking:boolean = false;
+
+  constructor(
+    private sharedService: SharedService,
+    private estateAgentFinderService: EstateAgentFinderService) {};
+  
+  ngOnInit() {
+    // The allocation of shared variables
+    this.towns = this.sharedService.towns;
+    this.heatingChoices = this.sharedService.heatingChoices;
+    this.aggregateStyleChoices = this.sharedService.aggregateStyleChoices;
+
+    this.town = this.towns[0];
+    this.heating = this.heatingChoices[0];
+    this.aggregateStyle = this.aggregateStyleChoices[0];
+  };
+  
+  setFeature(event, feature) {
+    if (feature=="heating") {
+      this.heating = event['target']['value'];
+    } else if (feature=="propertyStyle") {
+      this.aggregateStyle = event['target']['value'];
+    } else if (feature=="postcode") {
+      this.postcode = event['target']['value'];
+    } else if (feature=="bedrooms") {
+      this.bedrooms = Number(event['target']['value']);
+    } else if (feature=="address") {
+      this.address = event['target']['value'];
+    } else if (feature=="town") {
+      this.town = event['target']['value'];
+    };
+  };
+
+  setAmenityBool(amenity) {
+    if (amenity=="garage") {
+      this.garage = !this.garage;
+    } else if (amenity=="driveway") {
+      this.driveway = !this.driveway;
+    } else if (amenity=="garden") {
+      this.garden = !this.garden;
+    } else if (amenity=="bayWindow") {
+      this.bayWindow = !this.bayWindow;
+    } else if (amenity=="parking") {
+      this.parking = !this.parking;
+    };
+  };
+  
+  constructPropertyData() {
+    let propertyData = {
+      "propertyData": {
+        "address": this.address,
+        "town": this.town,
+        "postcode": this.postcode,
+        "aggregateStyle": this.aggregateStyle,
+        "details": {
+          "bedrooms": this.bedrooms,
+          "heating": this.heating,
+          "amenities": {
+            "garden": this.garden, 
+            "garage": this.garage, 
+            "driveway": this.driveway, 
+            "parking": this.parking, 
+            "bayWindow": this.bayWindow
+          }
+        }
+      }
+    };
+    return propertyData;
+  };
+
+  recommendEstateAgent() {
+    let propertyData = this.constructPropertyData();
+    console.log(propertyData);
+    this.estateAgentFinderService.estateAgentRecommender(propertyData)
+    .subscribe(estateAgentFinder => {
+      console.log(estateAgentFinder);
+    });
+  };
+}
---
+++ src/webserver/gui/estate_agent_finder/estate_agent_finder.service.ts
@@ -0,0 +1,28 @@
+import { Http, Response, Request, RequestOptions, RequestMethod } from "@angular/http";
+import { environment } from '../environment/environment';
+import { Injectable } from '@angular/core';
+
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class EstateAgentFinderService {
+  api:string = environment.API_HOST + "/api/v" +
+    environment.API_VERSION;
+
+  constructor(
+    private http: Http) {
+	};
+
+  estateAgentRecommender(propertyData) {
+    var requestoptions = new RequestOptions({
+			method: RequestMethod.Post,
+			url: this.api + '/estate_agent_recommender/',
+      body: propertyData
+		});
+		return this.http.request(
+      new Request(requestoptions))
+		.map(res => res.json());
+  };
+
+};
