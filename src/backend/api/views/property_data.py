a4e08e9fa7c3816bf873e1d535f36a2e78341a6c
O1sims <sims.owen@gmail.com>
2019-07-08 22:28:29 +0100

    Add endpoints and views for User

--- src/backend/api/views/property_data.py
+++ src/backend/api/views/property_data.py
@@ -1,3 +1,5 @@
+import json
+
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
@@ -8,6 +10,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from api.services.MongoService import MongoService
+from api.services.RedisService import RedisService
 from api.models.property_data import GeneratePropertyDataModel, PropertyDataModel
 
 from sukasa.config import MONGO_DB_INFO
@@ -24,14 +27,15 @@ SEARCH_Q = openapi.Parameter(
 def generate_brief(property_data):
     return "{} bed {} for sale".format(
         property_data['details']['bedrooms'],
-        property_data['details']['style']
+        property_data['details']['aggregateStyle']
     )
 
 def generate_tags(property_data):
     return [
         property_data['address'].lower(),
         property_data['postcode'].lower(),
-        property_data['town'].lower()
+        property_data['town'].lower(),
+        property_data['details']['longPostcode'].lower()
     ]
 
 class PropertyDataView(ListCreateAPIView):
@@ -71,13 +75,16 @@ class PropertyDataView(ListCreateAPIView):
             property_data=request.data)
         request.data['tags'] = generate_tags(
             property_data=request.data)
+        if RedisService().check_token(
+            redis_key=request.META.get('HTTP_TOKEN')):
+            user_info = json.loads(RedisService().getter(
+                redis_key=request.META.get('HTTP_TOKEN')))
+            request.data['owner'] = user_info['_id']
+        else:
+            return Response(status=500)
         MongoService().insert_to_collection(
-            collection_name=MONGO_DB_INFO['propertyCollection'],
+            collection_name=MONGO_DB_INFO['masterCollection'],
             data=request.data)
-        all_property_data = MongoService().get_from_collection(
-            collection_name=MONGO_DB_INFO['propertyCollection'])
-        create_property_estimation_model(
-            property_data=all_property_data)
         return Response(status=201)
 
 
--- src/backend/api/views/user.py
+++ src/backend/api/views/user.py
@@ -1,4 +1,7 @@
-import hashlib, binascii, os
+import os
+import json
+import hashlib
+import binascii
 
 from uuid import uuid4
 from drf_yasg.utils import swagger_auto_schema
@@ -7,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.generics import CreateAPIView
 
-from api.models.user import LoginModel, UserModel
+from api.models.user import LoginModel, UserModel, TokenModel
 from api.services.MongoService import MongoService
 from api.services.RedisService import RedisService
 
@@ -51,17 +54,35 @@ class LoginView(CreateAPIView):
         if len(users) == 1 and verify_password(
             stored_password=users[0]['password'], 
             provided_password=request.data['password']):
+            del(users[0]['password'])
             token = [str(uuid4()), "Successfully logged in!", 200]
             RedisService().set_token(
-                redis_key=token[0])
+                redis_key=token[0], 
+                value=json.dumps(users[0]))
         else:
-            token = [None, "Unable to log in. Please check username and password.", 403]
+            token = [None, "Unable to log in. Please check username and password.", 200]
         return Response(
             data={
+                "user": users,
                 "token": token[0],
                 "msg": token[1]
-            },
-            status=token[2])
+            }, status=token[2])
+
+
+class LogoutView(CreateAPIView):
+    renderer_classes = (JSONRenderer, )
+    serializer_class = TokenModel
+
+    @swagger_auto_schema(responses={200: "Success"})
+    def post(self, request, *args, **kwargs):
+        TokenModel(
+            data=request.data).is_valid(
+            raise_exception=True)
+        RedisService().delete_token(request.data['token'])
+        return Response(
+            data={
+                "msg": "Logged user out"
+            }, status=200)
 
 
 class UserView(CreateAPIView):
@@ -82,10 +103,39 @@ class UserView(CreateAPIView):
             MongoService().insert_to_collection(
                 collection_name=MONGO_DB_INFO['userCollection'],
                 data=request.data)
-            status = [True, "Successfully created User", 201]
+            del(request.data['password'])
+            status = [True, "Successfully created User", str(uuid4()), 201]
+            RedisService().set_token(
+                redis_key=status[2], 
+                value=json.dumps(request.data))
         else:
-            status = [False, "Could not create User", 403]
+            status = [False, "Could not create User", None, 401]
         return Response(
-            data={"success": status[0], "msg": status[1]},
-            status=status[2])
+            data={
+                "success": status[0], 
+                "msg": status[1], 
+                "token": status[2], 
+                "user": request.data},
+            status=status[3])
 
+
+class TokenView(CreateAPIView):
+    renderer_classes = (JSONRenderer, )
+    serializer_class = TokenModel
+        
+    @swagger_auto_schema(responses={200: "Success"})
+    def post(self, request, *args, **kwargs):
+        TokenModel(
+            data=request.data).is_valid(
+            raise_exception=True)
+        exists = RedisService().check_token(
+            redis_key=request.data['token'])
+        if exists:
+            userData = RedisService().getter(
+                redis_key=request.data['token'])
+            retData = [userData, "User found", 200]
+        else:
+            retData = [None, "User not found", 401]
+        return Response(
+            data={"user": json.loads(retData[0]), "msg": retData[1]},
+            status=retData[2])
