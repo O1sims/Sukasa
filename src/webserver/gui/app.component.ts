a822f52808c16988146e63d6f33e2248c24b4a80
O1sims <sims.owen@gmail.com>
2019-07-08 22:41:24 +0100

    Reformatting

--- src/webserver/gui/app.component.ts
+++ src/webserver/gui/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-
-
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 
 
-export class AppComponent {
-}
+export class AppComponent { }
